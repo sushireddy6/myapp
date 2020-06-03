afbfeb673b053a39821a09fc0bdd76d38e4153da
sushireddy6 <sushireddy94@gmail.com>
2020-06-03 01:48:48 -0700

    init commit

---
+++ build/precache-manifest.13ad64c59aa71f5b111b2e77b1b1d701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f18c4c3cf8a69667f6d3d8328339d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36b05c06d0ad06cc6efa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2919e725cbca7f857ff",
+    "url": "/static/js/2.e60888fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e60888fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b05c06d0ad06cc6efa",
+    "url": "/static/js/main.74faa342.chunk.js"
+  },
+  {
+    "revision": "25cd98df73b3d8a1c058",
+    "url": "/static/js/runtime-main.f9326a65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
